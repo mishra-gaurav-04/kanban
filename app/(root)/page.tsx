2af443eba2e0775efc4d1c20e070e960e1b7e5de
Gaurav Mishra <gaurav.info217@gmail.com>
2023-12-22 21:07:37 +0530

    updated page component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,104 @@
 import React from 'react'
 import Card from '@/components/shared/Card';
-const page = () => {
-  return (
-   <section className='wrapper'>
-    <div>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-    </div>
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
+import {grey} from '@mui/material/colors';
 
-   </section>
-  )
+const page = () => {
+    return (
+        <section className='bg-gray-200 py-10'>
+            <div className='wrapper'>
+                <div className='space-y-2'>
+                    <div className='flex items-center justify-between'>
+                        <div className='flex items-center gap-3'>
+                            <CheckCircleIcon sx={{color : grey[500]}}/>
+                            <h3>Backlog</h3>
+                            <p className='text-gray-500'>2</p>
+                        </div>
+                        <div>
+                            <AddOutlinedIcon sx={{color : grey[500]}}/>
+                            <MoreHorizOutlinedIcon sx={{color : grey[500]}}/>
+                        </div>
+                    </div>
+                    <div className='flex flex-col items-center gap-3'>
+                        <Card/>
+                        <Card/>
+                    </div>
+                </div> 
+                <div className='space-y-2'>
+                    <div className='flex items-center justify-between'>
+                        <div className='flex items-center gap-3'>
+                            <CheckCircleIcon sx={{color : grey[500]}}/>
+                            <h3>Backlog</h3>
+                            <p className='text-gray-500'>2</p>
+                        </div>
+                        <div>
+                            <AddOutlinedIcon sx={{color : grey[500]}}/>
+                            <MoreHorizOutlinedIcon sx={{color : grey[500]}}/>
+                        </div>
+                    </div>
+                    <div className='flex flex-col items-center gap-3'>
+                        <Card/>
+                        <Card/>
+                    </div>
+                </div>          
+                <div className='space-y-2'>
+                    <div className='flex items-center justify-between'>
+                        <div className='flex items-center gap-3'>
+                            <CheckCircleIcon sx={{color : grey[500]}}/>
+                            <h3>Backlog</h3>
+                            <p className='text-gray-500'>2</p>
+                        </div>
+                        <div>
+                            <AddOutlinedIcon sx={{color : grey[500]}}/>
+                            <MoreHorizOutlinedIcon sx={{color : grey[500]}}/>
+                        </div>
+                    </div>
+                    <div className='flex flex-col items-center gap-3'>
+                        <Card/>
+                        <Card/>
+                    </div>
+                </div> 
+                <div className='space-y-2'>
+                    <div className='flex items-center justify-between'>
+                        <div className='flex items-center gap-3'>
+                            <CheckCircleIcon sx={{color : grey[500]}}/>
+                            <h3>Backlog</h3>
+                            <p className='text-gray-500'>2</p>
+                        </div>
+                        <div>
+                            <AddOutlinedIcon sx={{color : grey[500]}}/>
+                            <MoreHorizOutlinedIcon sx={{color : grey[500]}}/>
+                        </div>
+                    </div>
+                    <div className='flex flex-col items-center gap-3'>
+                        <Card/>
+                        <Card/>
+                    </div>
+                </div> 
+                <div className='space-y-2'>
+                    <div className='flex items-center justify-between'>
+                        <div className='flex items-center gap-3'>
+                            <CheckCircleIcon sx={{color : grey[500]}}/>
+                            <h3>Backlog</h3>
+                            <p className='text-gray-500'>2</p>
+                        </div>
+                        <div>
+                            <AddOutlinedIcon sx={{color : grey[500]}}/>
+                            <MoreHorizOutlinedIcon sx={{color : grey[500]}}/>
+                        </div>
+                    </div>
+                    <div className='flex flex-col items-center gap-3'>
+                        <Card/>
+                        <Card/>
+                    </div>
+                </div> 
+            </div>
+            
+
+        </section>
+    )
 }
 
 export default page
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -6,27 +6,33 @@ import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
 
 const Header = () => {
-    const[toggle,setToggle] = useState<boolean>(false);
+    const [toggle, setToggle] = useState<boolean>(false);
     const handleToggleChange = () => {
         setToggle(!toggle);
     }
     return (
         <header className='w-full border-b-2 bg-white'>
             <div className='p-5 flex items-center justify-between'>
-                <div className='ml-3 border-2 border-gray-400 flex items-center gap-5 shadow-xl p-1 rounded-lg cursor-pointer' onClick={handleToggleChange}>
+                <div className='ml-3 border-2 border-gray-300 flex items-center gap-5 shadow-xl p-1 rounded-lg cursor-pointer' onClick={handleToggleChange}>
                     <Image src="/settings.svg" alt='settings' width={13} height={13} />
                     <p>Display</p>
                     {
-                        toggle ?  (<Image src="/down-arrow.svg" alt='up-arrow' width={13} height={13} />)
-                        :
-                        ( <Image src="/up-arrow.svg" alt='up-arrow' width={13} height={13} />)
+                        toggle ? (<Image src="/down-arrow.svg" alt='up-arrow' width={13} height={13} />)
+                            :
+                            (<Image src="/up-arrow.svg" alt='up-arrow' width={13} height={13} />)
                     }
                 </div>
+                { toggle && (
+                        <div className="absolute top-full left-0 bg-gray-500 shadow-lg rounded-md mt-2 p-3">
+                            Content to be displayed on toggle
+                        </div>
+                )}
                 <div>
-                   <DarkModeIcon />
+                    <DarkModeIcon />
                 </div>
             </div>
         </header>
+        
     )
 }
 
